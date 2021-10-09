84b87b013a4c8816b353e632568ada2b2e0070de
cclintris <181250083@smail.nju.edu.cn>
2021-10-09 10:14:21 +0800

    fix env TEST bug

--- src/constant/env.ts
+++ src/constant/env.ts
@@ -1,3 +1,3 @@
 import { EEnv } from '../model';
 
-export const DEFAULT_DEF = { appName: 'DEFAULT_APP_NAME', env: EEnv.TEST };
+export const DEFAULT_DEF = { appName: 'DEFAULT_APP_NAME', env: EEnv.PROD };
